f29706869a306cc5864871fd9efdd393162b704c
Luca <lucafagiolini@live.it>
2024-02-08 16:22:16 +0100

    palindroma start

---
+++ palindroma/js/script.js
@@ -0,0 +1,10 @@
+document.querySelector('#start-game').addEventListener('click', function () {
+    console.log('game started!');
+
+    let parola
+
+    parola = prompt('Inserisci una parola palindroma:');
+    console.log(parola);
+
+});
+
